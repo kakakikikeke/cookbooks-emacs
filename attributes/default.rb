e5e7f0784b70e497aa0e1b99c1948831fd09fc4c
root <root@vmyoshi-seed.(none)>
2013-04-30 00:13:30 +0900

    09 add init.d script of emacsd for centos.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 default['emacs']['dir'] = "/root/"
 default['emacs']['tar_save_dir'] = "/tmp/"
 default['emacs']['install']['full'] = false
-default['network'] = true
+default['network'] = false
+default['emacs']['path'] = "/usr/local/bin/"
+default['emacs']['uid'] = 0
