ac0dc386ddeb05a6d10327e87f09931beaa66c85
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-03-13 11:13:38 +0900

    fix comment .emacs and init.el

--- files/default/init_for_package.el
+++ files/default/init_for_package.el
@@ -1,4 +1,14 @@
+;;; .init.el --- init config emacs
+
+;;; Commentary:
+;;;; Author: kakakikikeke
+;;;; Version: 0.1
+
+;;; Code:
+; for package.el
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+;;; init.el ends here
