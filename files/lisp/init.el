e3a1d0438b11976a5cfd2495bf830fe9a9403ee9
kakakikikeke <cdl.yoshinaka@gmail.com>
2025-03-20 11:29:26 +0900

    Update files/lisp/init.el

--- files/lisp/init.el
+++ files/lisp/init.el
@@ -1,3 +1,13 @@
 ; for lisp
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
+(add-hook 'emacs-lisp-mode-hook 'elisp-autofmt-mode)
 (add-hook 'lisp-mode-hook 'company-mode)
+(add-hook 'lisp-mode-hook 'elisp-autofmt-mode)
+(defun my-elisp-before-save-hook ()
+  "Emacs Lisp のファイルを保存するときにのみ実行する before-save-hook."
+  (when (eq major-mode 'emacs-lisp-mode)
+    (message "Saving Emacs Lisp file: %s" buffer-file-name)
+    (elisp-autofmt-buffer)))
+
+;; フックの登録
+(add-hook 'before-save-hook #'my-elisp-before-save-hook)
